354586127e97e69cd29ce89eaa8fb06b26ad7fc5
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-11-18 10:52:32 +0000

    should lib added

--- test/index.js
+++ test/index.js
@@ -1,46 +1,37 @@
-var assert = require('assert');
+var should = require('should');
 var mp3dat = require('../index.js');
 var testFile = 'test/test.mp3';
 
-assert(mp3dat, 'mp3dat failed to load');
-assert(mp3dat.stat, 'there should be a stat method');
-assert(mp3dat.stat instanceof Function, 'stat should be a Function');
+should.exist(mp3dat);
+mp3dat.should.have.property('stat');
+mp3dat.stat.should.be.a.Function;
 
 mp3dat.stat(testFile, function(err,stats){
    
-   assert.ifError(err);
+   should.ifError(err);
    
    //expected properties
-   assert(stats.duration, 'should be a truthy duration property');
-   assert(stats.bitrate, 'should be a truthy bitrate property');
-   assert(stats.filesize, 'should be a truthy filesize property');
-   assert(stats.timestamp, 'should be a truthy timestamp property');
-   assert(stats.timesig, 'should be a truthy timesig property');
+   stats.should.have.property('duration');
+   stats.should.have.property('bitrate');
+   stats.should.have.property('filesize');
+   stats.should.have.property('timestamp');
+   stats.should.have.property('timesig');
    
    //expected types
-   assert.equal(typeof stats.duration, 'object', 'duration should be an object type');
-   assert(stats.duration instanceof Object, 'duration should be an instance of Object');
-   assert(!isNaN(stats.bitrate), 'bitrate should be a number ' );
-   assert(!isNaN(stats.filesize, 'filesize should be a number'));
-   assert(!isNaN(stats.timestamp), 'timestamp should be a number');
-   assert(stats.timesig.match(/^\d+:\d+:\d+$/), 'timesig should be in HH:MM:SS format');
+   stats.duration.should.be.an.Object;
+   stats.bitrate.should.be.a.Number;
+   stats.filesize.should.be.a.Number;
+   stats.timestamp.should.be.a.Number;
+   stats.timesig.should.match(/^\d+:\d+:\d+$/);
    
    //expected duration properties
-   assert.notStrictEqual(stats.duration.hours, undefined, 'should be a duration.hours property');
-   assert.notStrictEqual(stats.duration.minutes, undefined, 'should be a duration.minutes property');
-   assert.notStrictEqual(stats.duration.seconds, undefined, 'should be a duration.seconds property');
-   assert.notStrictEqual(stats.duration.miliseconds, undefined, 'should be a duration.miliseconds property');
+   stats.duration.should.have.keys('hours','minutes','seconds','miliseconds');
    
-   //expected duration types
-   assert(!isNaN(stats.duration.hours), 'duration.hours should be a number');
-   assert(!isNaN(stats.duration.minutes), 'duration.minutes should be a number');
-   assert(!isNaN(stats.duration.seconds), 'duration.seconds should be a number');
-   assert(!isNaN(stats.duration.miliseconds), 'duration.miliseconds should be a number');
-   
-   //expected duration properties constraints
-   assert(stats.duration.minutes < 60, 'duration.minutes should be no greater than 59');
-   assert(stats.duration.seconds < 60, 'duration.seconds should be no greater than 59');
-   assert(stats.duration.miliseconds < 1000, 'duration.miliseconds should be no greater than 999');
+   //expected duration types and constraines
+   stats.duration.hours.should.be.a.Number;
+   stats.duration.minutes.should.be.below(60).and.be.a.Number;
+   stats.duration.seconds.should.be.below(60).and.be.a.Number;
+   stats.duration.miliseconds.should.be.below(1000).and.be.a.Number;
    
    console.log('All tests passed'); 
 });
